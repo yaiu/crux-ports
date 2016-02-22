e206f1a49686fbcaf243742b4a0c43ad28396286
onodera-punpun <onodera@openmailbox.org>
2016-02-22 18:46:39 +0100

    netsurf: remove bashims in script.

--- netsurf/netsurf.sh
+++ netsurf/netsurf.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-shortname=`echo $LANG | cut -b1-2`
-if [[ -d /usr/share/netsurf/$shortname ]]; then
+shortname=`echo "$LANG" | cut -b1-2`
+if test -d /usr/share/netsurf/$shortname; then
 	/usr/bin/netsurf.elf "$@"
 else
 	LANG=en /usr/bin/netsurf.elf "$@"
